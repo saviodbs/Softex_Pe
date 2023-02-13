640211ceff717b3814624f5d232d6bbaa4931e5a
saviodbs <114783385+saviodbs@users.noreply.github.com>
2023-02-13 11:23:02 -0300

    Add files via upload

---
+++ Javascript_Mod2/funções.js
@@ -0,0 +1,9 @@
+function Function(){}
+
+    function somacomum (a,b){
+    return(a+b)}
+    
+    arrow = (a,b)=>{return a**b}
+console.log(somacomum(5,8))
+console.log(arrow(5,5))
+console.log(Function())
